6b9b6de550660c63b5f206adef84603dc8fc6736
Shiromaiker406 <kennethricaborda40@gmail.com>
2026-05-03 23:03:43 +0800

    Add files via upload

---
+++ QUEUEING/src/main/java/com/mycompany/queueing/QUEUEING.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.queueing;
+
+/**
+ *
+ * @author lucky
+ */
+
+public class QUEUEING {
+public static String username = "LaquiAng";
+public static String password = "12345";
+    public static void main(String[] args) {
+        new START_UP().setVisible(true);
+        
+    }
+}
